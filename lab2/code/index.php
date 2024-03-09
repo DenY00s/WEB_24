24dd816134239ea477277337bc14122ee11d8454
DenY00s <noviko8d@mail.ru>
2024-03-09 22:14:41 +0200

    added part 2 from laba2 by ND

--- lab2/code/index.php
+++ lab2/code/index.php
@@ -7,4 +7,19 @@ $order = &$very_bad_unclear_name;
 $order .= " and fries for Denis Novikov";
 echo "\nYour order is: $very_bad_unclear_name.";
 
+// Числа
+$integer_variable = 5;
+echo "<br>";
+echo $integer_variable;
+echo "\n";
+$float_variable = 3.14;
+echo $float_variable;
+echo "<br>";
+echo (10 + 2);
 
+// Расходы в прошлом и этом месяце
+$last_month = 1187.23;
+$this_month = 1089.98;
+
+$expenses_difference = $last_month - $this_month;
+echo "<br>Difference in expenses between last month and this month: $expenses_difference<br>";
